7f31990812a36bbb470e4138cca74cfc57396507
Alexandr <alexandrmail32@gmail.com>
2019-06-05 11:17:06 +0300

    Elimination of double function call

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 function daysInYear(year) {
   try {
-    if(!daysAmountInYear(year)) {
-      throw new TypeError("exeption");
-    } 
-      return daysAmountInYear(year);
-    } catch (error) {
-      return error.message;
-    }
-}
-
-function daysAmountInYear(year) {
-  if (Number.isInteger(year)) {
-    if (year > 0) {
-      const thisYearEnd = new Date(year + 1, 0, 1);
-      const thisYearstart = new Date(year, 0, 1);
-      const daysAmount = (thisYearEnd.getTime() - thisYearstart.getTime())/(3600000*24);
-      return daysAmount;
-    }
+    return daysAmountInYear(year);
+  } catch (error) {
+    return error.message;
   }
 }
 
-daysInYear(2019); // 365
-daysInYear('2019'); // exception
-daysInYear(2020); // 366
-daysInYear(2020.2); // exception
-daysInYear(2100); // 365
-daysInYear({year: 2100}); // exception
+function daysAmountInYear(year) {
+  if (Number.isInteger(year) && year > 0) {
+    const thisYearEnd = new Date(year + 1, 0, 1);
+    const thisYearStart = new Date(year, 0, 1);
+    const daysAmount = (thisYearEnd.getTime() - thisYearStart.getTime())/(3600000*24);
+    return daysAmount;
+  } else {
+    throw new TypeError("exeption");
+  }
+}
+
+console.log(daysInYear(2019)); // 365
+console.log(daysInYear('2019')); // exception
+console.log(daysInYear(2020)); // 366
+console.log(daysInYear(2020.2)); // exception
+console.log(daysInYear(2100)); // 365
+console.log(daysInYear({year: 2100})); // exception
